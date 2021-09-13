b09f17e8da7fed8a4e984803956a85d3b5094326
Adrien Pujol <ap@cdfr.net>
2021-09-13 16:47:25 +0100

    Add ESLint

---
+++ ui/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  ignorePatterns: [
+    '**/dist/**/*.js'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
